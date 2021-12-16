6c9453c616ca77215ae3b844a8b3b09fa86da37e
hwetsman <howard@tocdr.com>
2021-12-16 15:01:04 -0600

    Code: add total size

--- Mean_Diff.py
+++ Mean_Diff.py
@@ -16,6 +16,7 @@ def Means_of_Boots(array):
 
 def Confid_Levels(array, confidence):
     boot = Means_of_Boots(array)
+    plt.hist(boot)
     boot_lower = np.percentile(boot, confidence*100/2)
     boot_upper = np.percentile(boot, 100-confidence*100/2)
     return boot_lower, boot_upper
@@ -23,9 +24,10 @@ def Confid_Levels(array, confidence):
 
 # define 2 arrays
 # these will in the future be supplied by users
-array1 = np.random.randint(5, 15, 100)
+array1 = np.random.randint(6, 18, 100)
 array2 = np.random.randint(8, 22, 100)
 confidence = .05  # this is the alpha you wish to test
+total = array1.size + array2.size
 
 # define means
 mean1 = array1.mean()
@@ -41,9 +43,13 @@ print(boot2_lower, boot2_upper)
 fig, ax = plt.subplots()
 # array1
 ax.vlines(1, array1.min(), array1.max(), color='b')
-ax.hlines(array1.mean(), 1-.2, 1+.2, color='b')
+ax.hlines(array1.mean(), 1, 2, color='b')
+ax.hlines(boot1_lower, 1, 2, color='b')
+ax.hlines(boot1_upper, 1, 2, color='b')
 
 # array2
-ax.vlines(2, array2.min(), array2.max())
+ax.vlines(2, array2.min(), array2.max(), color='y')
+ax.hlines(boot2_lower, 1, 2, color='y')
+ax.hlines(boot2_upper, 1, 2, color='y')
 
 plt.show()
